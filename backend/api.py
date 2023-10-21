3a2973a6ca663858e11ba69367b86ecdf6068172
rialgou <rialgou@gmail.com>
2023-10-20 23:07:07 -0300

    agregamos supabase

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from supabase import create_client
+
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    response = supabase.table('Usuario').select("*").execute()
+    print(response)
+    return response
