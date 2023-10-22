1cec668ce6f88a9cf818410e3178e93f92256e15
mmolina2018 <mmolina2018@inf.udec.cl>
2023-10-21 21:12:18 -0300

    create app.py with supabase

---
+++ backend/src/view/app.py
@@ -0,0 +1,26 @@
+import os
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from .routes import router
+from prometheus_fastapi_instrumentator import Instrumentator
+from routes import router
+
+
+
+app = FastAPI()
+instrumentator = Instrumentator().instrument(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+
+@app.on_event("startup")
+async def _startup():
+    instrumentator.expose(app)
--- backend/src/view/routes.py
+++ backend/src/view/routes.py
@@ -1,19 +1,33 @@
-from fastapi import FastAPI
+from fastapi import Request, APIRouter
+from controller.labs import get_hours
 import os
 from dotenv import load_dotenv
-load_dotenv()
-
 from supabase import create_client
-
+load_dotenv()
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 supabase = create_client(url, key)
 
-app = FastAPI()
 
 
-@app.get("/")
+
+router = APIRouter()
+
+
+@router.get("/")
 async def root():
     response = supabase.table('Usuario').select("*").execute()
     print(response)
     return response
+
+
+@router.get("/hours/{labid}")
+def hours(
+    request: Request,
+    labid: str,
+    sid: str
+):
+    return get_hours(
+        labid=labid,
+        sid = sid,
+    )
