75d91ebb9300cb63bc88c8ef6bd19cb2967a6a9f
esuazo2601 <esteban.suazo.m2601@gmail.com>
2023-10-23 21:36:48 -0300

    Microorganismo routes are done

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -1,7 +1,9 @@
 from ..database.models import *
 from ..core.methods import *
-from fastapi import Request, APIRouter
+from fastapi import APIRouter
+from fastapi import HTTPException
 router = APIRouter()
+
 ################################### BLOQUES ROUTES ################################### 
 @router.get("/bloques")
 async def getBlocks():
@@ -39,3 +41,36 @@ async def getRoom():
 async def deleteRoom(sala_nombre:str):
     result = await delete_room(sala_nombre)
     return result
+
+################################### MICROORGANISMO ROUTES ################################### 
+@router.post("/microorganismo", status_code=201)
+async def addMicroorg(microorganismo:Microorganismo):
+    result = await add_microorg(microorganismo)
+    return result
+
+@router.get("/microorganismo/{nombre_cientifico}", status_code=302)
+async def getMicroorgCin(nombre_cientifico:str):
+    result = await get_microorg_cin(nombre_cientifico)
+    if len(result.data)==0:
+        raise HTTPException(status_code=404,detail="No se encontro el microorganismo")
+    
+    return result
+
+@router.get("/microorganismo/{nombre_comun}", status_code=302)
+async def getMicroorgCm(nombre_comun:str):
+    result = await get_microorg_cm(nombre_comun)
+    if len(result.data)==0:
+        raise HTTPException(status_code=404,detail="No se encontro el microorganismo")
+    
+
+@router.delete("/microorganismo/{id_microorg}", status_code=202)
+async def deleteMicroorg(id_microorg:int):
+    result = await delete_microorg(id_microorg)
+    return result
+
+@router.put("/microorganismo_update/{id}", status_code=202)
+async def updateMicroorg(id:int,nuevo:ActualizarMicroorganismo ):
+    result = await update_microorg(id,nuevo)
+    return result
+################################### INVESTIGACIÓN ROUTES ################################### 
+
--- backend/src/core/methods.py
+++ backend/src/core/methods.py
@@ -1,5 +1,6 @@
 from ..database.dbconfig import supabase
 from ..database.models import *
+from fastapi import HTTPException
 
 ################################### BLOQUES METHODS ################################### 
 async def get_blocks():
@@ -51,3 +52,48 @@ async def delete_room(sala_nombre:str):
         return {'message':f'Sala con nombre: {sala_nombre} fue borrada'}
     else:
         return {'message':'No se encuentra la sala con esta id'} 
+    
+################################### MICROORGANISMOS METHODS ################################### 
+
+async def add_microorg(microorganismo:Microorganismo):
+    response = supabase.table('Microorganismo').insert({
+        "nombre_cientifico":microorganismo.nombre_cientifico,
+        "nombre_comun":microorganismo.nombre_comun,
+        "procedencia":microorganismo.procedencia,
+        "detalles":microorganismo.detalles
+    }).execute()
+    return response
+
+async def delete_microorg(id:int):
+    response = supabase.table('Microorganismo').delete().eq('id',id).execute()
+    if response:
+        return response
+    else:
+        return {'message':f'No se encontró microorganismo de id: {id}'}
+    
+
+async def get_microorg_cm(nombre_comun:str):
+    response = supabase.table('Microorganismo').select('*').eq('nombre_comun',nombre_comun).execute()
+    return response
+
+async def get_microorg_cin(nombre_cientifico:str):
+    response = supabase.table('Microorganismo').select('*').eq('nombre_cientifico',nombre_cientifico).execute()
+    return response
+
+async def update_microorg(id:int,nuevo:ActualizarMicroorganismo):
+    print(f"id {id} , detalles {nuevo.detalles}, procedencia {nuevo.procedencia}")
+    if nuevo.procedencia and nuevo.detalles:
+        response = supabase.table('Microorganismo').update({"detalles":nuevo.detalles, "procedencia":nuevo.procedencia}).eq('id',id).execute()
+        return response
+    elif nuevo.detalles:
+        print("Entre aqui al else de detalles")
+        response = supabase.table('Microorganismo').update({"detalles":nuevo.detalles}).eq('id',id).execute()
+        return response
+    elif nuevo.procedencia:
+        response = supabase.table('Microorganismo').update({"procedencia":nuevo.procedencia}).eq('id',id).execute()
+        return response
+    else:
+        print("Entre aqui al execption")
+        raise HTTPException(status_code=406,detail="No se especifica que se cambia")
+
+################################### INVESTIGACION METHODS ################################### 
