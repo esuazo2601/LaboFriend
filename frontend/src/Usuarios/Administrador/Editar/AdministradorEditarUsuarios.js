8c3d75bd1d72a6de3f8c89c4234911f056dc76ab
rialgou <rialgou@gmail.com>
2023-11-28 02:19:12 -0300

    feat: eliminar usuario en vista administrador

--- frontend/src/Usuarios/Administrador/Editar/AdministradorEditarUsuarios.js
+++ frontend/src/Usuarios/Administrador/Editar/AdministradorEditarUsuarios.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table, Spinner } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 import '../Inventario/Estilos/tabla.css';
 import '../Inventario/Estilos/paginacion.css';
+import './Estilos/Spinner.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash, faSearch } from '@fortawesome/free-solid-svg-icons';
 import ModalEliminarConfirmar from './ModalEliminarConfirmar';
 import ModalEditarUsuario from './ModalEditarUsuario';
-import { getUsers,getUserByEmail,getUserScopes } from '../../../api_service/user_api';
+import { getUsers,getUserByEmail,getUserScopes, deleteUser } from '../../../api_service/user_api';
 
 function AdministradorInventario() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -16,7 +17,10 @@ function AdministradorInventario() {
   const [showModalEditar, setShowModalEditar] = useState(false);
   const [tipoUsuarioEditar, setTipoUsuarioEditar] = useState('');
   const [usuariosTabla, setUsuariosTabla] = useState([]);
-  
+  const [loading, setLoading] = useState(true);
+  const [refreshDelete, setRefreshDelete] = useState(false)
+
+
   useEffect(() => {
     try {
       const getData = async () => {
@@ -38,14 +42,18 @@ function AdministradorInventario() {
         } else {
           setUsuariosTabla([]);
         }
+        setLoading(false);
+        setRefreshDelete(false);
       };
   
       getData();
     } catch (error) {
       console.log("Se encontró un error:", error);
       setUsuariosTabla([]);
+      setLoading(false);
+      setRefreshDelete(false);
     }
-  }, []);
+  }, [refreshDelete]);
   
 
   console.log('Usuarios Tabla',usuariosTabla)
@@ -94,7 +102,15 @@ function AdministradorInventario() {
     setShowModalEliminar(true);
   }
 
-  const handleDelete = () => {
+  const handleDelete = async (email) => {
+    console.log(email)
+    try{
+      const resp = await deleteUser(email)
+      console.log(resp)
+      setRefreshDelete(true)
+    }catch(error){
+      console.log(error)
+    }
     console.log("Usuario eliminado"); 
   };
 
@@ -120,45 +136,73 @@ function AdministradorInventario() {
             <FontAwesomeIcon icon={faSearch} />
           </span>
         </div>
-        <Table striped bordered hover className="estilo-tabla">
-          <thead>
-            <tr>
-              <th className="encabezado-tabla text-center align-middle">Nombre</th>
-              <th className="encabezado-tabla text-center align-middle">Correo</th>
-              <th className="encabezado-tabla text-center align-middle">Tipo de Usuario</th>
-              <th colspan="2" className="encabezado-tabla text-center align-middle">Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            {paginatedUsuarios.map((usuario, index) => (
-              <tr key={index}>
-                <td className="columna-nombre-tabla text-center align-middle" >{usuario.nombre}</td>
-                <td className="celdas-restantes-tabla text-center align-middle">{usuario.email}</td>
-                <td className="celdas-restantes-tabla text-center align-middle">{usuario.tipo}</td>
-                <td className="celdas-restantes-tabla text-center align-middle" onClick={() => handleEditar(usuario)}><FontAwesomeIcon style={{cursor:'pointer'}} icon={faEdit} /></td>
-                <td className="celdas-restantes-tabla text-center align-middle" onClick={() => handleEliminarClick(usuario)}><FontAwesomeIcon style={{cursor:'pointer'}} icon={faTrash} /></td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
 
-        <div className="pagination">
-          <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-            Anterior
-          </button>
-          {Array.from({ length: totalPages }, (_, index) => index + 1).map((page, index) => (
-            <button
-              key={index}
-              onClick={() => setCurrentPage(page)}
-              className={currentPage === page ? "active" : ""}
-            >
-              {page}
-            </button>
-          ))}
-          <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-            Siguiente
-          </button>
-        </div>
+        {loading ? (
+          <div className='spinner-overlay'>
+            <Spinner animation="border" role="status" style={{ width: '4rem', height: '4rem' }}>
+            <span className="visually-hidden">Cargando...</span>
+          </Spinner>
+          </div>
+          
+        ) : (
+          <>
+            {totalItems === 0 ? (
+              <p className="text-center">No se han encontrado usuarios.</p>
+            ) : (
+              <Table striped bordered hover className="estilo-tabla">
+                <thead>
+                  <tr>
+                    <th className="encabezado-tabla text-center align-middle">Nombre</th>
+                    <th className="encabezado-tabla text-center align-middle">Correo</th>
+                    <th className="encabezado-tabla text-center align-middle">Tipo de Usuario</th>
+                    <th colSpan="2" className="encabezado-tabla text-center align-middle">
+                      Acción
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {paginatedUsuarios.map((usuario, index) => (
+                    <tr key={index}>
+                      <td className="columna-nombre-tabla text-center align-middle">{usuario.nombre}</td>
+                      <td className="celdas-restantes-tabla text-center align-middle">{usuario.email}</td>
+                      <td className="celdas-restantes-tabla text-center align-middle">{usuario.tipo}</td>
+                      <td
+                        className="celdas-restantes-tabla text-center align-middle"
+                        onClick={() => handleEditar(usuario)}
+                      >
+                        <FontAwesomeIcon style={{ cursor: 'pointer' }} icon={faEdit} />
+                      </td>
+                      <td
+                        className="celdas-restantes-tabla text-center align-middle"
+                        onClick={() => handleEliminarClick(usuario)}
+                      >
+                        <FontAwesomeIcon style={{ cursor: 'pointer' }} icon={faTrash} />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )}
+
+            <div className="pagination">
+              <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+                Anterior
+              </button>
+              {Array.from({ length: totalPages }, (_, index) => index + 1).map((page, index) => (
+                <button
+                  key={index}
+                  onClick={() => setCurrentPage(page)}
+                  className={currentPage === page ? 'active' : ''}
+                >
+                  {page}
+                </button>
+              ))}
+              <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                Siguiente
+              </button>
+            </div>
+          </>
+        )}
 
         {selectedUsuario && (
           <ModalEditarUsuario
@@ -168,16 +212,20 @@ function AdministradorInventario() {
             tipoUsuarioActual={tipoUsuarioEditar}
             onActualizarTipoUsuario={handleUpdateTipoUsuario}
           />
+          
         )}
-
-        <ModalEliminarConfirmar
+        {selectedUsuario && (
+          <ModalEliminarConfirmar
           show={showModalEliminar}
           onHide={() => setShowModalEliminar(false)}
           tipoElemento="Usuario"
           nombreElemento={selectedUsuario ? selectedUsuario.nombre : ''}
-          onDelete={handleDelete}
+          email={selectedUsuario ? selectedUsuario.email : ''}
+          onDelete={() => handleDelete(selectedUsuario? selectedUsuario.email: ' ')}
         />
-
+        )}
+        
+        
       </Container>
     </motion.div>
   );
