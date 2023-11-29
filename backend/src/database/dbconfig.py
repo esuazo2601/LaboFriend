21815b445dfdddf563baeb7da7564ecd9e688c3b
mmolina2018 <mmolina2018@inf.udec.cl>
2023-11-28 21:23:48 -0300

    create test dbconfig

--- backend/src/database/dbconfig.py
+++ backend/src/database/dbconfig.py
@@ -7,7 +7,22 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 # Carga las variables de entorno desde el archivo .env
 load_dotenv(dotenv_path)
 
+# Obtener el entorno actual (por defecto, establecerlo en "development" si no está definido)
+environment = os.environ.get("ENVIRONMENT", "development")
+
 url:str = os.environ.get("SUPABASE_URL")
 key:str = os.environ.get("SUPABASE_KEY")
 
-supabase : Client = create_client(url, key)
+
+
+# Ajustar la configuración según el entorno
+if environment == "test":
+    url = os.environ.get("TEST_SUPABASE_URL")
+    key = os.environ.get("TEST_SUPABASE_KEY")
+else:
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_KEY")
+
+
+# Crear cliente de Supabase
+supabase: Client = create_client(url, key)
