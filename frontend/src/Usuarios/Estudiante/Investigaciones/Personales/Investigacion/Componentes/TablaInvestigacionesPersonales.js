22ab45c89a8d6e79a5a628e3efb276428ff8539f
rialgou <rialgou@gmail.com>
2023-11-29 00:16:23 -0300

    feat: visualización y eliminación investigaciones personales en vista de estudiante

--- frontend/src/Usuarios/Estudiante/Investigaciones/Personales/Investigacion/Componentes/TablaInvestigacionesPersonales.js
+++ frontend/src/Usuarios/Estudiante/Investigaciones/Personales/Investigacion/Componentes/TablaInvestigacionesPersonales.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table,Spinner } from 'react-bootstrap';
 import '../Estilos/tabla-avances.css';
 import '../Estilos/paginacion.css';
 import '../../../../../../EstilosGlobales/basicos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
 import DetalleAvance from './ModalDetalle';
-import EliminarAvance from './ModalEliminacion';
+import EliminarInvestigacion from './ModalEliminacion';
 import { getTrabajandoEmail } from '../../../../../../api_service/investigaciones_api';
-import { getInvestigacionById } from '../../../../../../api_service/investigaciones_api';
+import { getInvestigacionById, deleteInvestigacion } from '../../../../../../api_service/investigaciones_api';
 
 const TablaInvestigacionesPersonales = ({ searchTerm }) => {
 
-    const [InvestigacionesPersonales,setInvestigacionesPersonales] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [InvestigacionesPersonales, setInvestigacionesPersonales] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [refreshDelete, setRefreshDelete] = useState(false)
+    const [showModalDetalle, setShowModalDetalle] = useState(false);
+    const [showModalEliminacion, setShowModalEliminacion] = useState(false);
+    const [selectedInvestigacion, setSelectedInvestigacion] = useState(null);
+
     useEffect(() => {
         try {
             const getData = async () => {
@@ -35,12 +40,13 @@ const TablaInvestigacionesPersonales = ({ searchTerm }) => {
                     }));
     
                     console.log('Investigacion list:', investigacion_list);
-    
                     setInvestigacionesPersonales(investigacion_list);
                     setLoading(false);
+                    setRefreshDelete(false);
                 } else {
                     setInvestigacionesPersonales([]);
                     setLoading(false);
+                    setRefreshDelete(false);
                 }
             };
     
@@ -54,20 +60,26 @@ const TablaInvestigacionesPersonales = ({ searchTerm }) => {
 
     console.log("Inv",InvestigacionesPersonales)
 
-    const [showModalDetalle, setShowModalDetalle] = useState(false);
-    const [showModalEliminacion, setShowModalEliminacion] = useState(false);
-    const [selectedAvance, setSelectedAvance] = useState(null);
 
     //Visualizacion de confirmacion de eliminacion
-    const handleEliminationClick = (avance) => {
+    const handleEliminationClick = (investigacion) => {
         setShowModalEliminacion(true);
-        setSelectedAvance(avance);
+        setSelectedInvestigacion(investigacion);
     };
 
     //Eliminar avance
-    const handleEliminarAvance = (avance) => {
-        setShowModalEliminacion(false);
-        setSelectedAvance(avance);
+    const handleEliminarInvestigacion = async(id) => {
+        console.log(id)
+        try{
+            const resp = await deleteInvestigacion(id)
+            console.log(resp)
+            setRefreshDelete(true)
+          }catch(error){
+            console.log(error)
+          }
+          console.log("Investigación eliminada");
+        //MANEJAR ELIMINACION DE AVNACE CON LLAMADA A API
+        // await deleteInvestigacion(avance.id)
     };
 
     const normalizeText = (text) => {
@@ -112,62 +124,73 @@ const TablaInvestigacionesPersonales = ({ searchTerm }) => {
     };
 
     return (
-
         <div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th className="encabezado-tabla text-center align-middle" style={{ width: '5%' }}>#</th>
-                        <th className="encabezado-tabla text-center align-middle" style={{ width: '40%' }}>Nombre</th>
-                        <th className="encabezado-tabla text-center align-middle" style={{ width: '10%' }}>Fecha</th>
-                        <th colspan="2" className="encabezado-tabla text-center align-middle" style={{ width: '5%' }}>Acción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {paginatedAvances.map((investigacion, index) => (
-                        <tr key={index}>
-                            <td className="columna-nombre-tabla text-center align-middle">{investigacion.id}</td>
-                            <td className="celdas-restantes-tabla text-center align-middle">{investigacion.titulo}</td>
-                            <td className="celdas-restantes-tabla text-center align-middle">{formatFecha(investigacion.fecha)}</td>
-                            <td className="celdas-restantes-tabla opcion-accion text-center align-middle" onClick={() => window.location.href = '/ayudante/investigaciones/personales/investigacion'}><FontAwesomeIcon icon={faEye} /></td>
-                            <td className="celdas-restantes-tabla opcion-accion text-center align-middle" onClick={() => handleEliminationClick(investigacion)}><FontAwesomeIcon icon={faTrash} style={{ color: "red" }} /></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-            <div className="pagination">
-                <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-                    Anterior
-                </button>
-                {Array.from({ length: totalPages }, (_, index) => index + 1).map((page, index) => (
-                    <button
-                        key={index}
-                        onClick={() => setCurrentPage(page)}
-                        className={currentPage === page ? "active" : ""}
-                    >
-                        {page}
-                    </button>
-                ))}
-                <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-                    Siguiente
-                </button>
-            </div>
+            {loading ? (
+                <div className='loading-spinner'>
+                    <Spinner animation="border" role="status" size="lg">
+                    <span className="visually-hidden">Cargando...</span>
+                    </Spinner>
+                </div>
+                
+            ) : (
+                <div>
+                    {InvestigacionesPersonales.length === 0 ? (
+                        <p>No hay investigaciones disponibles.</p>
+                    ) : (
+                        <>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th className="encabezado-tabla text-center align-middle" style={{ width: '5%' }}>#</th>
+                                        <th className="encabezado-tabla text-center align-middle" style={{ width: '40%' }}>Nombre</th>
+                                        <th className="encabezado-tabla text-center align-middle" style={{ width: '10%' }}>Fecha</th>
+                                        <th colspan="2" className="encabezado-tabla text-center align-middle" style={{ width: '5%' }}>Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {paginatedAvances.map((investigacion, index) => (
+                                        <tr key={index}>
+                                            <td className="columna-nombre-tabla text-center align-middle">{investigacion.id}</td>
+                                            <td className="celdas-restantes-tabla text-center align-middle">{investigacion.titulo}</td>
+                                            <td className="celdas-restantes-tabla text-center align-middle">{formatFecha(investigacion.fecha)}</td>
+                                            <td className="celdas-restantes-tabla text-center align-middle" onClick={() => window.location.href = '/ayudante/investigaciones/terceros/investigacion'}><FontAwesomeIcon style={{cursor:'pointer'}} icon={faEye} /></td>
+                                            <td className="celdas-restantes-tabla text-center align-middle" onClick={() => handleEliminationClick(investigacion)}><FontAwesomeIcon icon={faTrash} style={{ color: "red" , cursor: 'pointer'}} /></td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                            <div className="pagination">
+                                <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+                                    Anterior
+                                </button>
+                                {Array.from({ length: totalPages }, (_, index) => index + 1).map((page, index) => (
+                                    <button
+                                        key={index}
+                                        onClick={() => setCurrentPage(page)}
+                                        className={currentPage === page ? "active" : ""}
+                                    >
+                                        {page}
+                                    </button>
+                                ))}
+                                <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+                                    Siguiente
+                                </button>
+                            </div>
 
-
-            <DetalleAvance
-                show={showModalDetalle}
-                avance={selectedAvance}
-                onHide={() => setShowModalDetalle(false)}
-            />
-            <EliminarAvance
-                show={showModalEliminacion}
-                avance={selectedAvance}
-                onHide={() => setShowModalEliminacion(false)}
-                onEliminarAvance={handleEliminarAvance}
-            />
-        </div >
-
-
+                            <DetalleAvance
+                                onHide={() => setShowModalDetalle(false)}
+                            />
+                            <EliminarInvestigacion
+                                show={showModalEliminacion}
+                                id={selectedInvestigacion? selectedInvestigacion.id:''}
+                                onHide={() => setShowModalEliminacion(false)}
+                                onDelete={handleEliminarInvestigacion}
+                            />
+                        </>
+                    )}
+                </div>
+            )}
+        </div>
     );
 };
 
