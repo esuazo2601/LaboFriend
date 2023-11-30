72b1e3d92518601828bbd863b3851493daab1f2b
mmolina2018 <mmolina2018@inf.udec.cl>
2023-11-29 21:08:24 -0300

    black formatting

--- backend/src/database/dbconfig.py
+++ backend/src/database/dbconfig.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from supabase import create_client, Client
 
 # Ruta relativa al archivo .env en la carpeta "database"
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 # Carga las variables de entorno desde el archivo .env
 load_dotenv(dotenv_path)
 
@@ -12,15 +12,14 @@ environment = os.environ.get("ENVIRONMENT", "development")
 
 # Ajustar la configuración según el entorno
 if environment == "test":
-    url:str = os.environ.get("SUPABASE_TEST_URL")
-    key:str = os.environ.get("SUPABASE_TEST_KEY")
+    url: str = os.environ.get("SUPABASE_TEST_URL")
+    key: str = os.environ.get("SUPABASE_TEST_KEY")
 else:
-    url:str = os.environ.get("SUPABASE_URL")
-    key:str = os.environ.get("SUPABASE_KEY")
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
 
 print(url)
 print(key)
-
 
 
 # Crear cliente de Supabase
