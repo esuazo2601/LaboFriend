cf997ca47577989dedbb23364ecbee5b2822b16c
rialgou <rialgou@gmail.com>
2023-12-12 21:08:37 -0300

    feat: boton de cerrar sesión en vista de los usuarios

--- frontend/src/Usuarios/Administrador/Cuenta/AdministradorCuenta.js
+++ frontend/src/Usuarios/Administrador/Cuenta/AdministradorCuenta.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
 function AdministradorCuenta() {
-    return (
-      <h1 style={{textAlign:'center'}} >En construcción . . .</h1>
-    );
-  }
-  
-  export default AdministradorCuenta;
+  const handleCerrarSesion = () => {
+    // Limpiar el localStorage al cerrar sesión
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
+    localStorage.removeItem("email");
+    localStorage.removeItem("scopes");
+
+    // Redireccionar a la página de inicio de sesión
+    window.location.href = "/"; // Cambia esto según la ruta de tu página de inicio de sesión
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center  vh-100">
+      <h1 className="mb-4">En construcción . . .</h1>
+      <Button variant="primary" onClick={handleCerrarSesion}>
+        Cerrar Sesión
+      </Button>
+    </div>
+  );
+}
+
+export default AdministradorCuenta;
